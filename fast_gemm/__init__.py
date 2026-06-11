764228e1605fb85582ab575be2d00ca92bb4db95
rhsieh <richard.hsieh@eridu.ai>
2026-06-11 11:24:22 -0700

    added function wrapper

--- fast_gemm/__init__.py
+++ fast_gemm/__init__.py
@@ -1,15 +1,16 @@
 import torch
+import functools
 import fast_gemm_cuda
 
-def matmul_naive(A, B, C=None, alpha=1.0, beta=0.0):
-  if C is None:
-    C = torch.zeros((A.shape[0], B.shape[1]), device=A.device, dtype=A.dtype)
+def auto_allocate_c(cuda_func):
+  """Decorator to auto allocate output tensor C if not provided."""
+  @functools.wrap(cuda_func)
+  def wrapper(A, B, C=None, alpha=1.0, beta=0.0):
+    if C is None:
+      C = torch.zeros((A.shape[0], B.shape[1]), device=A.device, dtype=A.dtype)
+    return cuda_func(A, B, C, alpha, beta)
+  return wrapper
 
-  return fast_gemm_cuda.matmul_naive(A, B, C, alpha, beta)
-
-
-def matmul_global_coalesced(A, B, C=None, alpha=1.0, beta=0.0):
-  if C is None:
-    C = torch.zeros((A.shape[0], B.shape[1]), device=A.device, dtype=A.dtype)
-
-  return fast_gemm_cuda.matmul_global_coalesced(A, B, C, alpha, beta)
+# expose functions
+matmul_naive = auto_allocate_c(fast_gemm_cuda.matmul_naive)
+matmul_global_coalesced = auto_allocate_c(fast_gemm_cuda.matmul_global_coalesced)
